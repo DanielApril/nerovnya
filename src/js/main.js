e3a82fd53bdc41fe69791a46a6c2f12977ed1efb
Daniel Pasko <d.pasko@pnlfin.tech>
2021-05-05 22:55:20 +0500

    add footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,20 @@
 let $main = $('main');
 let $pageHeader = $('.page-header');
+let $pageFooter = $('.page-footer');
 let $menuOpenBtn = $('.menu-open-button');
 let $asideEl = $('aside');
 
 function openMenu() {
 	$main.addClass('_menu-opened');
 	$pageHeader.addClass('_menu-opened');
+	$pageFooter.addClass('_menu-opened');
 	$asideEl.addClass('_active');
 }
 
 function closeMenu() {
 	$main.removeClass('_menu-opened');
 	$pageHeader.removeClass('_menu-opened');
+	$pageFooter.removeClass('_menu-opened');
 	$asideEl.removeClass('_active');
 }
 
