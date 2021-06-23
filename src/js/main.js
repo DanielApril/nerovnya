5989feb3062b27dd3eea599319fd8e8cb9b8a38f
Daniel Pasko <d.pasko@pnlfin.tech>
2021-06-23 14:31:29 +0500

    isotope is now alive

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ let $asideOpenEl = document.querySelector('.open-menu');
 let $asideCloseEl = document.querySelector('.overlay-aside-close');
 let $menuCloseBtn = document.querySelector('.close-menu');
 let $bodyEl = document.querySelector('body');
+let $projectsListEl = document.querySelector('.projects-list .inner');
 
 function openMenu() {
 	$asideEl.classList.add('_active');
@@ -42,3 +43,58 @@ const indexSlider = new Swiper('.index-slider', {
 		crossFade: true
 	},
 });
+
+
+// init Isotope
+var iso = new Isotope($projectsListEl, {
+	itemSelector: '.project',
+	layoutMode: 'fitRows',
+	fitRows: {
+		gutter: 20
+	}
+});
+
+// filter functions
+var filterFns = {
+	// show if number is greater than 50
+	numberGreaterThan50: function (itemElem) {
+		var number = itemElem.querySelector('.number').textContent;
+		return parseInt(number, 10) > 50;
+	},
+	// show if name ends with -ium
+	ium: function (itemElem) {
+		var name = itemElem.querySelector('.name').textContent;
+		return name.match(/ium$/);
+	}
+};
+
+// bind filter button click
+var filtersElem = document.querySelector('.project__filters');
+filtersElem.addEventListener('click', function (event) {
+	// only work with buttons
+	if (!matchesSelector(event.target, 'button')) {
+		return;
+	}
+	var filterValue = event.target.getAttribute('data-filter');
+	// use matching filter function
+	filterValue = filterFns[filterValue] || filterValue;
+	iso.arrange({ filter: filterValue });
+});
+
+// change is-checked class on buttons
+var buttonGroups = document.querySelectorAll('.project__filters');
+for (var i = 0, len = buttonGroups.length; i < len; i++) {
+	var buttonGroup = buttonGroups[i];
+	radioButtonGroup(buttonGroup);
+}
+
+function radioButtonGroup(buttonGroup) {
+	buttonGroup.addEventListener('click', function (event) {
+		// only work with buttons
+		if (!matchesSelector(event.target, 'button')) {
+			return;
+		}
+		buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
+		event.target.classList.add('is-checked');
+	});
+}
